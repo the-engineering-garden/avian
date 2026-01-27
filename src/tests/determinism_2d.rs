21c1e15a5dfc1440178f65894865e1f935328eee
Joona Aalto <jondolf.dev@gmail.com>
2026-01-28 00:15:46 +0200

    Update determinism hash

--- src/tests/determinism_2d.rs
+++ src/tests/determinism_2d.rs
@@ -60,7 +60,7 @@ fn cross_platform_determinism_2d() {
     let hash = compute_hash(app.world(), query);
 
     // Update this value if simulation behavior changes.
-    let expected = 0x8111821f;
+    let expected = 0x2696dd94;
 
     assert!(
         hash == expected,
